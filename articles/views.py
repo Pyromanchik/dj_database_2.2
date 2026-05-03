fbe42b0053192065c66ff84cc1e34a5d045aed05
Pyromanchik <romanap2902@gmail.com>
2026-05-03 18:12:01 +0400

    views.py исправлен

--- articles/views.py
+++ articles/views.py
@@ -5,17 +5,15 @@ from .models import Article
 def articles_list(request):
     template = 'articles/news.html'
 
-    # используйте этот параметр для упорядочивания результатов
     ordering = '-published_at'
 
-    # Получаем все статьи с предзагрузкой связанных тегов через Scope,
-    # чтобы избежать N+1 запросов и обеспечить нужную сортировку
+
     articles = Article.objects.prefetch_related(
         'scopes__tag'
     ).order_by(ordering)
 
     context = {
-        'object_list': articles  # Именно так ожидается в шаблоне (object_list)
+        'object_list': articles
     }
 
     return render(request, template, context)
